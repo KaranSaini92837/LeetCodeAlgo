60f67b5dc4be64648cb09112a86ee74bb3c80d9b
KaranSaini92837 <37518846+KaranSaini92837@users.noreply.github.com>
2019-07-28 14:30:21 +0530

    Leetcode #917

--- src/LeetCode/ReverseOnlyLetters.java
+++ src/LeetCode/ReverseOnlyLetters.java
@@ -1,9 +1,27 @@
 package LeetCode;
-
 public class ReverseOnlyLetters {
+    private static String reverseOnlyLetters(String S) {
+        StringBuilder sb = new StringBuilder(S);
+        int low = 0;
+        int high = sb.length() - 1;
 
-    public String reverseOnlyLetters(String S) {
-        return null;
+        while(low < high) {
+            int x = sb.charAt(low);
+            int y = sb.charAt(high);
+            if(x >= 33 && x < 58 || x >= 58 && x < 65 || x >= 91 && x < 97){
+                low++;
+                continue;
+            }
+            else if(y >= 33 && y < 58 || y >= 58 && y < 65 || y >= 91 && y < 97){
+                high--;
+                continue;
+            }
+            char tmp = sb.charAt(low);
+            sb.setCharAt(low, sb.charAt(high));
+            sb.setCharAt(high, tmp);
+            low++;
+            high--;
+        }
+        return sb.toString();
     }
-
 }
